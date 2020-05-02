3cf8c7cff00f3353da44577c1ddb639bc76c5f3a
rmit-s3668759-casey-coulter <s3668759@student.rmit.edu.au>
2020-05-02 23:27:15 +1000

    tutorial 7 done

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   post  '/login',   to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
   resources :users
-   
-  get 'hello' => 'hello#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
